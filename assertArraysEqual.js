60057e45d6d41530bf1c8105b620da01b6db8702
Kyle Liang <kliang1194@gmail.com>
2021-10-26 19:50:08 +0000

    update all files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,9 +8,9 @@ const eqArrays = function(arr1, arr2) {
 
   const assertArraysEqual = function (arr1, arr2) {
     if(eqArrays(arr1, arr2)) {
-      console.log(`✅✅✅ Assertion Paased: ${arr1} === ${arr2}`);
+      return console.log(`✅✅✅ Assertion Paased: ${arr1} === ${arr2}`);
     } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+      return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
     };
   }
 
--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Paased: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Paased: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
