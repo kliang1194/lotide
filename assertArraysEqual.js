4e7bf41778c6ab61a72958b05e24866923276535
Kyle <kliang1194@gmail.com>
2021-10-27 14:37:08 -0400

    add countLetter function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,7 +8,7 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    return console.log(`✅✅✅ Assertion Paased: ${arr1} === ${arr2}`);
+    return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
