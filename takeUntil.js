523c58967893e96533309dea22d2663519a4caea
Kyle <kliang1194@gmail.com>
2021-10-28 14:24:28 -0400

    create function to takeUntil condition

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    } else {
+      results.push(item);
+    }
+  } return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+/* Expecte output: [ 1, 2, 5, 7, 2 ]
+---
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+*/
+
+const eqArrays = function(arr1, arr2) {
+  for (let a = 0; a < arr1.length; a++) {
+    if (arr1[a] !== arr2[a]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
