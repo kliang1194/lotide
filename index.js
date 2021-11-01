9706762ccf001ad3f5820653f703866be08a8759
Kyle <kliang1194@gmail.com>
2021-11-01 12:29:33 -0400

    export all functions in lotide

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetter = require('./countLetter');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
 
 
 module.exports = {
@@ -10,5 +19,14 @@ module.exports = {
   tail:   tail,
   middle: middle,
   assertarraysEqual: assertArraysEqual,
-  assertEqual: assertEqual
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetter: countLetter,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map
 };
