1b9d107c031be3ffccbab22dfbcd799b40785f77
Kyle <kliang1194@gmail.com>
2021-11-01 10:04:18 -0400

    add mocha and tai testing to tailTest.js and middleTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,9 @@
-const assertEqual = require('../assertEqual');
-const tail = require ('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs,'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+});
