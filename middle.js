84c38b38db4f898209ebf6aeaaf970c1b2129edc
Kyle <kliang1194@gmail.com>
2021-10-31 22:58:01 -0400

    refactor middle function and isolate test cases

--- middle.js
+++ middle.js
@@ -1,19 +1,3 @@
-const eqArrays = function(actual, expected) {
-  for (let a = 0; a < actual.length; a++) {
-    if (actual[a] !== actual[a]) {
-      return false;
-    }
-  } return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(array) {
   let output = []
   if (array.length <= 2) {
@@ -26,11 +10,5 @@ const middle = function(array) {
   } return output;
 };
 
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle; 
